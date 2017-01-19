4668ae0ed793519d057e04b23bbbab319922b771
Matt Finucane <matfin@gmail.com>
2017-01-19 10:16:14 +0100

    Adding demo functions to main.c

--- DataStructuresAlgorithms/main.c
+++ DataStructuresAlgorithms/main.c
@@ -11,35 +11,87 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main(int argc, const char * argv[]) {
-
-    struct Person *head = setup("Head", "Person", 1);
-    push(&head, "Second", "Person", 2);
-    push(&head, "Third", "Person", 3);
-    push(&head, "Fourth", "Person", 4);
+/**
+ *  Demonstrating using push() to add items to 
+ *  the beginning of a linked list.
+ */
+int listPushDemo() {
     
+    struct Person *head = setup("Head", "Item", 1);
+    push(&head, "Second", "Pushed", 2);
+    push(&head, "Third", "Pushed", 3);
+    push(&head, "Fourth", "Pushed", 4);
+    
+    printf("The list now has %i items pushed.\n", listSize(head));
     printItems(head);
+    printf("\n\n");
     
-    struct Person *empty_head = NULL;
-    push(&empty_head, "One", "Empty", 1);
-    push(&empty_head, "Two", "Empty", 2);
+    push(&head, "Fifth", "Pushed", 5);
+    push(&head, "Sixth", "Pushed", 6);
+    push(&head, "Seventh", "Pushed", 7);
     
-    printItems(empty_head);
-    
-    struct Person *first_null = setup("Item", "One", 1);
-    struct Person *second_null = setup("Second", "Item", 2);
-    
-    changeToNull(&first_null);
-    changeToNull(&second_null);
-    
-    struct Person *append_head = setup("Tail", "Head", 1);
-    append(&append_head, "Tail", "Added", 2);
-    append(&append_head, "Added", "Tailagain", 3);
-    append(&append_head, "Atthe", "Tailend", 4);
-    
-    printItems(append_head);
-    
-    struct Person *copied = copyList(append_head);
+    printf("The list now has %i items pushed.\n", listSize(head));
+    printItems(head);
+    printf("\n\n");
     
     return 0;
+}
+
+/**
+ *  Demonstrating using append() to add items 
+ *  to the end of a linked list.
+ */
+int listAppendDemo() {
+    
+    struct Person *head = setup("Head", "Item", 1);
+    append(&head, "Second", "Appended", 2);
+    append(&head, "Third", "Appended", 3);
+    append(&head, "Fourth", "Appended", 4);
+    
+    printf("The list now has %i items appended.\n", listSize(head));
+    printItems(head);
+    printf("\n\n");
+    
+    append(&head, "Fifth", "Appended", 5);
+    append(&head, "Sixth", "Appended", 6);
+    append(&head, "Seventh", "Appended", 7);
+    
+    printf("The list now has %i items.\n", listSize(head));
+    printItems(head);
+    printf("\n\n");
+    
+    return 0;
+}
+
+/**
+ *  Demonstrating the listCopy() function to make a
+ *  fresh copy of a linked list.
+ */
+int listCopyDemo() {
+    
+    struct Person *head = setup("Head", "Item", 1);
+    append(&head, "Second", "Original", 2);
+    append(&head, "Third", "Original", 2);
+    append(&head, "Fourth", "Original", 2);
+    append(&head, "Fifth", "Original", 2);
+    
+    printf("This is the original linked list with %i items.\n", listSize(head));
+    printItems(head);
+    printf("\n\n");
+    
+    struct Person *copy = copyList(head);
+    printf("This is the copied linked list with %i items.\n", listSize(copy));
+    printItems(copy);
+    printf("\n\n");
+    
+    return 0;
+}
+
+int main(int argc, const char * argv[]) {
+
+    int a = listPushDemo();
+    int b = listAppendDemo();
+    int c = listCopyDemo();
+    
+    return (a + b + c);
 }
